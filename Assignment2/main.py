92e19fee0c5fcc3b4c06cd8d4c938403ba359c7e
SimonAldridge <saldri10@charlotte.edu>
2024-09-23 13:28:38 -0400

    testing something

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,8 +6,8 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(data.resources)
+cashier_instance = Cashier()
 
 
 
@@ -15,5 +15,5 @@ cashier_instance = ######
 def main():
     ###  write the rest of the codes ###
 
-if __name__=="__main__":
-    main()
+    if __name__=="__main__":
+        main()
