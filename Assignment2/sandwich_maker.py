3db9d5a605e4a20d931878ee80e9aed8fed317e9
SimonAldridge <saldri10@charlotte.edu>
2024-09-25 11:31:41 -0400

    Sadnwich maker class for modular sandwich maker

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -1,11 +1,20 @@
 
 class SandwichMaker:
-    def __init__(self, resources):
-        self.machine_resources = resources
+    def __init__(self):
+        pass
 
-    def check_resources(self, ingredients):
+    def check_resources(self, size, resources, recipes):
         """Returns True when order can be made, False if ingredients are insufficient."""
         #####
+        for key, amount in resources.items():
+                if resources.get(key, 0) < recipes[size]["ingredients"].get(key, 0):
+                     print(f"Sorry, not enough {key} to fulfill the order.\n")
+                     return False
+        return True
 
-    def make_sandwich(self, sandwich_size, order_ingredients):
+    def make_sandwich(self, size, resources, recipes):
         ########
+        ingredients = recipes[size]["ingredients"]
+        for ingredient, amount in ingredients.items():
+                resources[ingredient] -= amount
+        print(f"One {size} sandwich made! Resources updated and ready for next order.")
